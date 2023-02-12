64bd070186d952d42e8095fc76371679547e9984
komlaRD <ankueric1@gmail.com>
2023-02-12 18:21:54 +0000

    add a class for dishes in the component

---
+++ conFusion/src/app/shared/dish.ts
@@ -0,0 +1,22 @@
+export class Dish {
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+
+    constructor() {
+        this.id = " ";
+        this.name = " ";
+        this.image = " ";
+        this.category = " ";
+        this.featured = false;
+        this.label = " ";
+        this.price = " ";
+        this.description = " ";
+    }
+
+}
